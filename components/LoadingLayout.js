db948b2a8c5a4d2b249887809bf50ff5a7c32ad8
gianicola <gianicolajara@gmail.com>
2022-05-29 20:55:10 -0400

    fix loading layout

--- components/LoadingLayout.js
+++ components/LoadingLayout.js
@@ -7,21 +7,28 @@ let interval = null;
 
 const LoadingLayout = ({ children }) => {
   const [loading, setLoading] = useState(loadingInitial);
+  const [visible, setVisible] = useState(false);
 
   Router.onRouteChangeStart = () => {
-    setInterval(25);
+    setLoading(15);
+    setVisible(true);
+
+    if (interval) clearInterval(interval);
+
     interval = setInterval(() => {
-      setLoading((loading) => (loading >= 75 ? loading + 0 : loading + 5));
-    }, 2000);
+      setLoading((loading) => (loading >= 95 ? loading + 0 : loading + 5));
+    }, 250);
     document.body.style.overflowY = "hidden";
   };
 
   Router.onRouteChangeComplete = () => {
     clearInterval(interval);
     setLoading(100);
+
     setTimeout(() => {
-      setLoading(0);
-    }, 2500);
+      setVisible(false);
+    }, 1000);
+
     document.body.style.overflowY = "auto";
   };
 
@@ -30,14 +37,16 @@ const LoadingLayout = ({ children }) => {
     setLoading(0);
   };
 
+  const visibleCss = visible ? "visible" : "hidden";
+
   return (
     <div>
       <div
-        className={`fixed max-w-[100%] top-0 left-0 z-[9999999999999999] bg-sky-400`}
+        className={`${visibleCss} fixed max-w-[100%] top-0 left-0 z-[9999999999999999] bg-sky-400 shadow-sky-400/80 transition-all`}
         style={{
           width: `${loading}%`,
-          height: `${loading === 100 ? "3px" : "0px"}`,
-          transition: `width 2s ease-in-out`,
+          height: `${loading <= 100 ? "3px" : "0px"}`,
+          transition: `width 1s ease-in-out`,
         }}
       ></div>
       {children}
